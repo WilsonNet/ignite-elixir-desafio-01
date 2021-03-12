74634a5ed7605ed1b807ae3fe4e892cc1c9fe850
Wilson Neto <wilsonpsn@hotmail.com>
2021-03-12 18:14:25 -0300

    Tested function

--- test/list_length_test.exs
+++ test/list_length_test.exs
@@ -2,7 +2,15 @@ defmodule ListLengthTest do
   use ExUnit.Case
   doctest ListLength
 
-  test "greets the world" do
-    assert ListLength.hello() == :world
+  describe "call/1" do
+    test "returns the list length" do
+    list = [1, 8, 3]
+
+    response = ListLength.call(list)
+
+    expect_response = 3
+
+    assert response == expect_response
+    end
   end
 end
